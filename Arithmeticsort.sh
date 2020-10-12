a834f74f3d8f0e985084dcbffca40ac64d4f9b63
sourabhprasad11 <sourabh.prasad444@gmail.com>
2020-10-12 12:20:31 +0530

    Computing a+b*c

--- Arithmeticsort.sh
+++ Arithmeticsort.sh
@@ -2,6 +2,9 @@
 
 echo "Welcome to the Arithmetic and Sorting Computation"
 
-read -p "Enter a value" a
-read -p "Enter b value" b
-read -p "Enter c value" c
+read -p "Enter a value: " a
+read -p "Enter b value: " b
+read -p "Enter c value: " c
+
+uc1=$(( $a+$b*$c ));
+echo "the value of UC1: $uc1"
